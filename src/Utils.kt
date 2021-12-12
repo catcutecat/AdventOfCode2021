93cdb3aa7ac58f8cb35a1d47a14126c3d476271b
catcutecat <alisaxdgogogo@gmail.com>
2021-12-12 13:50:16 +0800

    Move input files from src/ to input/

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,7 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String) = File("input", "$name.txt").readLines()
 
 fun readInputAsInts(name: String) = readInput(name).map { it.toInt() }
 
